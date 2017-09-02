06d23ecd0af69e96bb1ac2f3012380ec568ab1f2
Andrea <andry25.9.99@gmail.com>
2017-09-02 23:47:15 +0200

    Add files via upload

--- assets/js/telegrambot.js
+++ assets/js/telegrambot.js
@@ -128,7 +128,7 @@ function NewResponseHandler(response){
                 //                                                  color                       update number                                                                        group/chat ID                                                                                      Person ID                                     Person Nickname                                       First name                                      Corpo messaggio
                     $('#chatHistory > tbody').prepend("<tr class=\"" + rowClass  +"\"><td>"+response.result[i].update_id+"</td><td><a href=\"#\" onclick=\"replyToId(event)\" value=\""+response.result[i].message.chat.id+"\">"+response.result[i].message.chat.id+"</a></td><td><a href=\"#\" onclick=\"#\" value=\""+response.result[i].message.from.id+"\" >@"+response.result[i].message.from.username+"</a></td><td>"+response.result[i].message.from.first_name+"</td><td>"+response.result[i].message.text+"</td></tr>");
                     if(!jlPage){
-                        if($("#notificationCheckbox").attr("checked")){
+                        if($("#notificationCheckbox input").attr("checked")){
                             var notification = new Notification('New mesage from '+response.result[i].message.from.username,{
                                 body:response.result[i].message.text,
                                 icon:'https://pbs.twimg.com/profile_images/519176711393406977/m6BFtJQW_400x400.png',
