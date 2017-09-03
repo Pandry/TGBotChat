a12f2d3ae3be8cc8497db126eaeb36015b50aeed
Andrea <andry25.9.99@gmail.com>
2017-09-03 14:12:30 +0200

    Improved comments

--- assets/js/telegrambot.js
+++ assets/js/telegrambot.js
@@ -22,14 +22,10 @@ Notification.requestPermission(function(p){
     }
 });
 
-
-
-
-
-
-
+//Set the Telegram bot API token
 function setApiToken(forced){
     if (typeof(Storage) !== "undefined") {
+        //Save token in local storage
         if(localStorage.getItem("botToken") == null || forced ){
             swal({
                 title: "Gimme the token!",
@@ -55,8 +51,9 @@ function setApiToken(forced){
         }else{
             botToken=localStorage.getItem("botToken");
         }
-        // Code for localStorage/sessionStorage.
+        
     } else {
+        //LocalStorage not abailable
         swal({
             title: "Gimme the token!",
             text: "Write me the token pliz:",
@@ -79,16 +76,17 @@ function setApiToken(forced){
     }
 }
 
-
+//Function invoked to send a message...
+//Should be improved passing the text to be sent as a parameter
 function sendMessage(){
     var chatMessage = $("#textMessage").val();
     var chatId = $("#chatId").val();
-    
+    //Check if message is empty
     if(chatMessage==""){
         swal("Attention please :3!", "You could have forgot to enter the message :3", "info");
         return;
     }
-    
+    //Sending
     var HttpMessageSender = new XMLHttpRequest();
     HttpMessageSender.open("GET", "https://api.telegram.org/bot"+botToken+"/sendMessage?chat_id="+chatId+"&text="+encodeURI(chatMessage), true); // true for asynchronous 
     HttpMessageSender.onreadystatechange = function () {
@@ -107,9 +105,11 @@ function sendMessage(){
     
     //Clear Text area
     $("#textMessage").val("");
+    //Save last chat ID
     localStorage.setItem("lastChatID", chatId);
 }
 
+//Function used to check and write down incoming messages
 function checkNewMessages(){
     var HttpMessageSender = new XMLHttpRequest();
     var updateURL = "https://api.telegram.org/bot"+botToken+"/getUpdates";
@@ -117,6 +117,7 @@ function checkNewMessages(){
     HttpMessageSender.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var response = JSON.parse(this.responseText);
+            //Call to a function that handles and write down in the page new messages
             NewResponseHandler(response);
         }else if (HttpMessageSender.readyState === XMLHttpRequest.DONE){
             console.error("Damn! An error occourred.");
@@ -127,60 +128,55 @@ function checkNewMessages(){
 }
 
 function NewResponseHandler(response){
-    //$('#myTable tr:last').after("<tr><td>1</td><td>Column content</td><td>Column content</td><td>Column content</td><td>Column content</td></tr>");
-        for(var i = 0; i < response.result.length; i++){
-            if(response.result[i].update_id > lastUpdateId){
-                
-                var rowClass;
-                if(response.result[i].edited_message == undefined){
-                    if(response.result[i].message.chat.id<0){rowClass = "table-info";}else{rowClass = "table-success";} 
-                //                                                  color                       update number                                                                        group/chat ID                                                                                      Person ID                                     Person Nickname                                       First name                                      Corpo messaggio
+    for(var i = 0; i < response.result.length; i++){
+        if(response.result[i].update_id > lastUpdateId){
+            var rowClass;
+            //Check if the message is edited or not
+            if(response.result[i].edited_message == undefined){
+                //Check if the message comes from a group chat
+                //Need to be improved by checking tif the message is private or not
+                //Assign a blue background if the message is from a group, a geen one if it's from a user via direct chat
+                if(response.result[i].message.chat.id<0){rowClass = "table-info";}else{rowClass = "table-success";} 
                     $('#chatHistory > tbody').prepend("<tr class=\"" + rowClass  +"\"><td>"+response.result[i].update_id+"</td><td><a href=\"#\" onclick=\"replyToId(event)\" value=\""+response.result[i].message.chat.id+"\">"+response.result[i].message.chat.id+"</a></td><td><a href=\"#\" onclick=\"replyToId(event)\" value=\""+response.result[i].message.from.id+"\" >@"+response.result[i].message.from.username+"</a></td><td>"+response.result[i].message.from.first_name+"</td><td>"+response.result[i].message.text+"</td></tr>");
                     if(!jlPage){
-                        if($("#notificationCheckbox input").attr("checked") == "checked"){
-                            var notification = new Notification('New mesage from '+response.result[i].message.from.username,{
-                                body:response.result[i].message.text,
-                                icon:'https://pbs.twimg.com/profile_images/519176711393406977/m6BFtJQW_400x400.png',
-                                onshow: function(){setTimeout(notification.close, 1500);}
-                            });
-                        }
+                    if($("#notificationCheckbox input").attr("checked") == "checked"){
+                    var notification = new Notification('New mesage from '+response.result[i].message.from.username,{
+                    body:response.result[i].message.text,
+                    icon:'https://pbs.twimg.com/profile_images/519176711393406977/m6BFtJQW_400x400.png',
+                    onshow: function(){setTimeout(notification.close, 1500);}
+                    });
+                    }
                 }
-
-                }else{
-                    //Edited message
-                    $('#chatHistory > tbody').prepend("<tr class=\"table-warning\"><td>"+response.result[i].update_id+"</td><td><a href=\"#\" onclick=\"replyToId(event)\" value=\""+response.result[i].edited_message.chat.id+"\">"+response.result[i].edited_message.chat.id+"</a></td><td><a href=\"#\" onclick=\"replyToId(event)\" value=\""+response.result[i].edited_message.from.id+"\" >@"+response.result[i].edited_message.from.username+"</a></td><td>"+response.result[i].edited_message.from.first_name+"</td><td>"+response.result[i].edited_message.text+"</td></tr>");
-                }
+            }else{
+                //Edited message
+                $('#chatHistory > tbody').prepend("<tr class=\"table-warning\"><td>"+response.result[i].update_id+"</td><td><a href=\"#\" onclick=\"replyToId(event)\" value=\""+response.result[i].edited_message.chat.id+"\">"+response.result[i].edited_message.chat.id+"</a></td><td><a href=\"#\" onclick=\"replyToId(event)\" value=\""+response.result[i].edited_message.from.id+"\" >@"+response.result[i].edited_message.from.username+"</a></td><td>"+response.result[i].edited_message.from.first_name+"</td><td>"+response.result[i].edited_message.text+"</td></tr>");
             }
         }
-        
+    }
     lastUpdateId = response.result[response.result.length-1].update_id;
     jlPage = false;
 }
 
-/*
-function clearRecentChats(event){
-    event.preventDefault();
-    $('#chatHistory > tbody >tr').remove();
-}*/
-
+//Clear the message hostory
+//Needs to be fixed
 $('#clearRecentChats').click(function(){
     $('#chatHistory > tbody').html(''); 
    });
 
+//Function used to replying to a user clicking on its username or ID
 function replyToId(event){
     $("#chatId").val(event.toElement.getAttribute("value"));
 }
 
 
-
-var botToken;
-var lastUpdateId =11;//Update per get 
-setApiToken();
+var botToken;//var containing the bot token
+var lastUpdateId =11;//Last update id received
+setApiToken();//Try to set the token if not setted (it's not forced)
 
 //Check message interval
 setInterval(checkNewMessages, 500);
 
-
+//Handles notification 
 $('#notificationCheckbox input').change(function() {
     if(!isNotificationsAllowed){
         Notification.requestPermission(function(p){
