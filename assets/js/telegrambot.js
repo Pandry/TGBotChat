a70ff001f9db76e72ad8242829b50c8c1b0b38a7
Andrea <andry25.9.99@gmail.com>
2017-09-03 13:49:23 +0200

    Update telegrambot.js

--- assets/js/telegrambot.js
+++ assets/js/telegrambot.js
@@ -90,7 +90,7 @@ function sendMessage(){
         if(HttpMessageSender.readyState === XMLHttpRequest.DONE && HttpMessageSender.status === 200) {
             swal("Sweet!", "Message sent.", "success");
         }else if(HttpMessageSender.readyState === XMLHttpRequest.DONE && HttpMessageSender.status === 403) {
-            swal("Damn!", "The user blocked the bot T.T", "warn");
+            swal("Damn!", "The user blocked the bot T.T", "warning");
         }else if (HttpMessageSender.readyState === XMLHttpRequest.DONE){
             swal("Damn!", "An error occourred.", "error");
         }
