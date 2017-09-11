7bbe77712744455ed4b64e527cc91cd54f77b44c
Andrea <andry25.9.99@gmail.com>
2017-09-11 12:38:34 +0200

    Update telegrambot.js

--- assets/js/telegrambot.js
+++ assets/js/telegrambot.js
@@ -106,7 +106,7 @@ function checkNewMessages() {
                 var response = JSON.parse(this.responseText);
                 if (response.ok == true) {
                     thisBot = response.result;
-                    $("#sendingAs").innerHTML("(Sending messages as <a href=\"https://t.me/" + thisBot.username + "\">\"@" + thisBot.username + "</a>)");
+                    $("#sendingAs").html("(Sending messages as <a href=\"https://t.me/" + thisBot.username + "\">\"@" + thisBot.username + "</a>)");
                 }
             }
         };
